92c43b61641c78845fc561313db3948779842c55
hemanth110702 <mamidihemanthkumar2002@gmail.com>
2024-02-16 11:17:30 +0530

    created and connect to db in mongoDB

--- backend/routes/workoutRoutes.js
+++ backend/routes/workoutRoutes.js
@@ -1,9 +1,25 @@
 const express = require("express");
+const Joi = require("joi");
+const Workout = require("../models/workoutModel");
 
 const router = express.Router();
 
-router.post("/", (req, res) => {
-  res.json({ msg: "This is an workoutRoute" });
+router.use(express.json());
+
+router.post("/", async (req, res) => {
+  try {
+    const { error } = validateWorkout(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const { title, reps, load } = req.body;
+    const workout = await Workout.create({ title, reps, load });
+    return res.status(200).json(workout);
+  } catch (err) {
+    console.error(err);
+    return res
+      .status(500)
+      .send("There was an internal server error while saving the file");
+  }
 });
 
 router.get("/", (req, res) => {
@@ -21,5 +37,14 @@ router.delete("/:id", (req, res) => {
 router.patch("/:id", (req, res) => {
   res.json({ msg: "This is an workoutRoute" });
 });
+
+function validateWorkout(data) {
+  const joiSchema = Joi.object({
+    title: Joi.string().required(),
+    load: Joi.number().required(),
+    reps: Joi.number().required(),
+  });
+  return joiSchema.validate(data);
+}
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workoutRoutes");
 
 const app = express();
@@ -16,7 +17,10 @@ app.get("/", (req, res) => {
 });
 app.use("/api/workouts", workoutRoutes);
 
-// listen for requests
-app.listen(process.env.PORT, () =>
-  console.log("Server is listening on port: " + process.env.PORT)
-);
+mongoose.connect(process.env.mongodb_URL).then(()=>{
+  // listen for requests
+  app.listen(process.env.PORT, () =>
+    console.log("Connected to DB & Server is listening on port: " + process.env.PORT)
+  );
+  
+}).catch();
