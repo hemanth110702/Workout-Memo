2d2c639593b9e39a685f847e2acd28ef8da92098
hemanth110702 <mamidihemanthkumar2002@gmail.com>
2024-02-16 15:04:37 +0530

    added frontend intial files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+function App() {
+
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
