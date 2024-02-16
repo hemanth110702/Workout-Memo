2674d58e3588ac045fdaaf69607187fe2872e911
hemanth110702 <mamidihemanthkumar2002@gmail.com>
2024-02-16 14:42:45 +0530

    added workoutController to handle workoutRoutes

--- backend/routes/workoutRoutes.js
+++ backend/routes/workoutRoutes.js
@@ -1,50 +1,24 @@
 const express = require("express");
-const Joi = require("joi");
-const Workout = require("../models/workoutModel");
+const {
+  createWorkout,
+  getWorkout,
+  getWorkouts,
+  deleteWorkout,
+  updateWorkout,
+} = require("../controller/workoutController");
 
 const router = express.Router();
 
 router.use(express.json());
 
-router.post("/", async (req, res) => {
-  try {
-    const { error } = validateWorkout(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+router.post("/", createWorkout);
 
-    const { title, reps, load } = req.body;
-    const workout = await Workout.create({ title, reps, load });
-    return res.status(200).json(workout);
-  } catch (err) {
-    console.error(err);
-    return res
-      .status(500)
-      .send("There was an internal server error while saving the file");
-  }
-});
+router.get("/", getWorkouts);
 
-router.get("/", (req, res) => {
-  res.json({ msg: "This is an workoutRoute" });
-});
+router.get("/:id", getWorkout);
 
-router.get("/:id", (req, res) => {
-  res.json({ msg: "This is an workoutRoute" });
-});
+router.delete("/:id", deleteWorkout);
 
-router.delete("/:id", (req, res) => {
-  res.json({ msg: "This is an workoutRoute" });
-});
-
-router.patch("/:id", (req, res) => {
-  res.json({ msg: "This is an workoutRoute" });
-});
-
-function validateWorkout(data) {
-  const joiSchema = Joi.object({
-    title: Joi.string().required(),
-    load: Joi.number().required(),
-    reps: Joi.number().required(),
-  });
-  return joiSchema.validate(data);
-}
+router.patch("/:id", updateWorkout);
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workoutRoutes");
+const errorHandler = require("./middleware/error-handler");
 
 const app = express();
 
@@ -17,10 +18,19 @@ app.get("/", (req, res) => {
 });
 app.use("/api/workouts", workoutRoutes);
 
-mongoose.connect(process.env.mongodb_URL).then(()=>{
-  // listen for requests
-  app.listen(process.env.PORT, () =>
-    console.log("Connected to DB & Server is listening on port: " + process.env.PORT)
-  );
-  
-}).catch();
+mongoose
+  .connect(process.env.mongodb_URL)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () =>
+      console.log(
+        "Connected to DB & Server is listening on port: " + process.env.PORT
+      )
+    );
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+    process.exit(1);
+  });
+
+app.use(errorHandler);
