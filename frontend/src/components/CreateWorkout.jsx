1f97a7c56a6598854a9f4114f6c8d0ae7201efd6
hemanth110702 <mamidihemanthkumar2002@gmail.com>
2024-02-19 08:23:43 +0530

    added error input error handling

--- frontend/src/components/CreateWorkout.jsx
+++ frontend/src/components/CreateWorkout.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import apiClient from "../services/apiClient";
 import { useWorkoutsContext } from "../context/WorkoutsContext";
 
@@ -8,24 +8,45 @@ const CreateWorkout = () => {
   const [load, setLoad] = useState("");
   const [reps, setReps] = useState("");
   const [error, setError] = useState(null);
+  const [emptyFields, setEmptyFields] = useState([]);
+
+  useEffect(() => {
+    if (title && emptyFields.includes("title"))
+      setEmptyFields((prev) => prev.filter((d) => d != "title"));
+    if (load && emptyFields.includes("load"))
+      setEmptyFields((prev) => prev.filter((d) => d != "load"));
+    if (reps && emptyFields.includes("reps"))
+      setEmptyFields((prev) => prev.filter((d) => d != "reps"));
+    if (title && load && reps) setError("");
+  }, [title, load, reps]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const workout = { title, load, reps };
-    try {
-      const response = await apiClient.post(
-        "/api/workouts",
-        JSON.stringify(workout)
-      );
-      setTitle("");
-      setLoad("");
-      setReps("");
-      setError(null);
-      dispatch({type: "CREATE_WORKOUT", payload: response.data})
-      console.log("new workout added ", response.data);
-    } catch (err) {
-      console.log(err);
-      setError("Error Adding Workout: \n" + err.response.data + "\n");
+
+    if (!title) setEmptyFields((prev) => [...prev, "title"]);
+    if (!load) setEmptyFields((prev) => [...prev, "load"]);
+    if (!reps) setEmptyFields((prev) => [...prev, "reps"]);
+    if (emptyFields.length > 0) {
+      setError("All Fields are required");
+      return
+    } else {
+      try {
+        const response = await apiClient.post(
+          "/api/workouts",
+          JSON.stringify(workout)
+        );
+        setTitle("");
+        setLoad("");
+        setReps("");
+        setError(null);
+        setEmptyFields([]);
+        dispatch({ type: "CREATE_WORKOUT", payload: response.data });
+        console.log("new workout added ", response.data);
+      } catch (err) {
+        console.log(err);
+        setError("Error Adding Workout: \n" + err.response.data + "\n");
+      }
     }
   };
 
@@ -36,12 +57,14 @@ const CreateWorkout = () => {
         type="text"
         value={title}
         onChange={(e) => setTitle(e.target.value)}
+        className={emptyFields.includes("title") ? "error" : ""}
       />{" "}
       <br />
       Load:{" "}
       <input
         type="number"
         value={load}
+        className={emptyFields.includes("load") ? "error" : ""}
         onChange={(e) => setLoad(e.target.value)}
       />{" "}
       <br />
@@ -49,6 +72,7 @@ const CreateWorkout = () => {
       <input
         type="number"
         value={reps}
+        className={emptyFields.includes("reps") ? "error" : ""}
         onChange={(e) => setReps(e.target.value)}
       />{" "}
       <br />
