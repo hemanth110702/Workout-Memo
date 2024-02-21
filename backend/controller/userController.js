6d8f48f6120c2f7d11145c9bb8b4e14b1c3c8c0e
hemanth110702 <mamidihemanthkumar2002@gmail.com>
2024-02-21 14:40:22 +0530

    added bcrypt for signup functionality

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,5 +1,6 @@
 const Joi = require("joi");
 const joiPwdComplexity = require("joi-password-complexity");
+const User = require("../models/userModel");
 
 const login = (req, res) => {
   const { error } = validateDetails(req.body);
@@ -9,12 +10,18 @@ const login = (req, res) => {
   res.status(200).send("login user");
 };
 
-const signup = (req, res) => {
+const signup = async (req, res) => {
   const { error } = validateDetails(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  const { email, password } = req.body;
-  res.status(200).send("signup user");
+  try {
+    const { email, password } = req.body;
+    const result = await User.signup(email, password);
+    res.status(200).send(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error.message);
+  }
 };
 
 function validateDetails(data) {
@@ -25,4 +32,4 @@ function validateDetails(data) {
   return joiSchema.validate(data);
 }
 
-module.exports = {login, signup}
+module.exports = { login, signup };
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = mongoose.Schema(
   {
@@ -9,5 +10,19 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+userSchema.statics.signup = async function (email, password) {
+  const exists = await this.findOne({ email });
+  if (exists) throw new Error("Email already in use");
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+    const user = await this.create({ email, password: hash });
+    return user;
+  } catch (err) {
+    throw err
+  }
+};
 
 module.exports = mongoose.model("User", userSchema);
