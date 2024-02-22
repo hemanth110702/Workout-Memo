e82e85115b20296111acbf47ac44f50cff776eba
hemanth110702 <mamidihemanthkumar2002@gmail.com>
2024-02-22 09:43:16 +0530

    added base url to make requests

--- frontend/src/services/apiClient.js
+++ frontend/src/services/apiClient.js
@@ -1,8 +1,18 @@
 import axios from "axios";
 
+const baseURLs = {
+  development: "http://localhost:4000",
+  production: "https://workout-memo-api.onrender.com",
+};
+
+const baseURL =
+  process.env.NODE_ENV === "production"
+    ? baseURLs.production
+    : baseURLs.development;
+
 export default axios.create({
-  baseURL: "http://localhost:4000", // backend server
+  baseURL,
   headers: {
-    "Content-Type": "application/json", 
+    "Content-Type": "application/json",
   },
 });
